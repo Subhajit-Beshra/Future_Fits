f605bd25887715a0a3415d5391f134342e0cbba2
Subhajit Beshra <subhajitbeshra21@gmail.com>
2026-04-09 11:58:42 +0530

    Added new files again!

---
+++ Contact.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
+  });
+}, {});
+
+elements.forEach(el => observer.observe(el));
