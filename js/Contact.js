cb2bba4ad8b0859525a55537b500a3b3cb9ab337
Subhajit Beshra <subhajitbeshra21@gmail.com>
2026-05-03 22:26:45 +0530

    Contact Form is done successfully!

--- js/Contact.js
+++ js/Contact.js
@@ -11,3 +11,45 @@ const observer = new IntersectionObserver((entries) => {
 }, {});
 
 elements.forEach(el => observer.observe(el));
+/*main logic for the contact page, this is where the form will be handled and the email will be sent to the server*/
+const form = document.getElementById('contactForm');
+const sendBtn = document.getElementById('sendBtn');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  sendBtn.innerText = 'Sending...';
+  sendBtn.disabled = true;
+  const formData = new FormData(form);
+
+  try{
+    const response = await fetch('https://formspree.io/f/mzdodqrd', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+
+    if(response.ok){
+      sendBtn.innerText = 'Message Sent!';
+      form.reset();
+      setTimeout(() => {
+        sendBtn.innerText = 'Send Message';
+        sendBtn.disabled = false;
+      }, 3000);
+    }
+    else{
+      sendBtn.innerText = 'Failed to Send';
+      setTimeout(() => {
+        sendBtn.innerText = 'Send Message';
+        sendBtn.disabled = false;
+      }, 3000);
+    }
+  }catch(error){
+    console.error('Error sending message:', error);
+    sendBtn.innerText = 'Error Sending';
+    sendBtn.disabled = false;
+  }
+
+})
