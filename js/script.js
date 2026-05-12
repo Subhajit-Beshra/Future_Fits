6aff3ef86d19a11d0e70ca0ccae760bcec629173
Subhajit Beshra <subhajitbeshra21@gmail.com>
2026-05-12 20:27:22 +0530

    Added new feature

--- js/script.js
+++ js/script.js
@@ -12,6 +12,58 @@ const observer = new IntersectionObserver((entries) => {
 
 elements.forEach(el => observer.observe(el));
 
+const products = document.querySelector(".products");
+
+Promise.all([
+fetch("https://dummyjson.com/products/category/mens-shirts?limit=3")
+.then(res => res.json()),
+
+fetch("https://dummyjson.com/products/category/womens-dresses?limit=3")
+.then(res => res.json()),
+
+fetch("https://dummyjson.com/products/category/mens-shoes?limit=3")
+.then(res => res.json()),
+
+fetch("https://dummyjson.com/products/category/womens-shoes?limit=3")
+.then(res => res.json()),
+
+fetch("https://dummyjson.com/products/category/sunglasses?limit=3")
+.then(res => res.json()),
+
+fetch("https://dummyjson.com/products/category/womens-bags?limit=3")
+.then(res => res.json())
+])
+
+.then(data => {
+  const allProducts = data.flatMap(item => item.products);
+  console.log(allProducts);
+  allProducts.forEach(product => {
+    products.innerHTML += `
+
+        <div class="card" data-id="${product.id}">
+
+        <img src="${product.thumbnail}" width="150">
+
+        <h3>${product.title}</h3>
+
+        <p>price: $${product.price}</p>
+
+      </div>
+    `;
+  });
+  document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", () => {
+      const id = card.dataset.id;
+      window.location.href = `pages/Product/Product.html?id=${id}`;
+    });
+  });
+});
+
+const showProduct = document.getElementById("showProduct");
+showProduct.addEventListener("click", () => {
+  window.location.href = "pages/Shop/Shop.html";
+})
+
 document.getElementById('shopBtn').addEventListener('click', () => {
-    window.location.href = "pages/Shop/Shop.html";
+  window.location.href = "pages/Shop/Shop.html";
 });
